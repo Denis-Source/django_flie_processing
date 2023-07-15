5364d78f5bf7ece83a8b071055eaa22368d8eec2
Denis <denis.zol@code-care.pro@gmail.com>
2023-07-16 01:15:01 +0300

    add docker support

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import sys
 from daphne.cli import CommandLineInterface
 
 if __name__ == "__main__":
-    sys.argv = ["daphne", "core.asgi:application"]
+    sys.argv = ["daphne", "core.asgi:application", "-b", "0.0.0.0", "-p", "8000"]
     CommandLineInterface.entrypoint()
