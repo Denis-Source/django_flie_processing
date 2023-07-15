3a6643957201e0d59313ed0d2d63fec3907aa401
Denis <denis.zol@code-care.pro@gmail.com>
2023-07-15 17:36:48 +0300

    make ws url the same as http ones

--- api/v1/task/ws_urls.py
+++ api/v1/task/ws_urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 from api.v1.task.consumers import TaskConsumer
 from websocket.middlewares import TokenAuthMiddleware
 
-urlpatterns = URLRouter([
+urlpatterns = [
     path("", TokenAuthMiddleware(TaskConsumer.as_asgi()))
-])
+]
--- core/asgi.py
+++ core/asgi.py
@@ -4,7 +4,9 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
-from api.v1.task.ws_urls import urlpatterns
+# from core.ws_urls import urlpatterns
+# from api.v1.task.ws_urls import urlpatterns
+from core.ws_urls import urlpatterns
 
 os.environ.setdefault("BASE_PATH", os.path.dirname(os.path.dirname(__file__)))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
@@ -13,5 +15,5 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
     "websocket":
-        URLRouter([path("ws/", urlpatterns)])
+        URLRouter([path("", URLRouter(urlpatterns))])
 })
