896c33ec2163089557354f732f6944305c0a7f95
Denis <denis.zol@code-care.pro@gmail.com>
2023-08-13 20:26:12 +0300

    add proper filters

--- api/v1/clipboard/filters.py
+++ api/v1/clipboard/filters.py
@@ -5,8 +5,16 @@ class CharInFilter(BaseInFilter, CharFilter):
     pass
 
 
+class CaseInsensitivePartialMatchCharFilter(CharFilter):
+    def filter(self, qs, value):
+        if value:
+            self.lookup_expr = "icontains"
+        return super().filter(qs, value)
+
+
 class ClipBoardFilter(FilterSet):
-    status = CharInFilter(field_name="media_type", lookup_expr="in")
+    media_type = CharInFilter(field_name="media_type", lookup_expr="in")
+    name = CaseInsensitivePartialMatchCharFilter(field_name="name")
     ordering = OrderingFilter(
         fields=(
             ("auto_delete", "auto_delete"),
